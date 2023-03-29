831672f82d4c1f1a430e1b005e2edc32abb5c12f
Vignesh-1704 <vigneshmanikavasagam17@gmail.com>
2023-03-29 16:06:38 +0530

    Add. Included the Logic And Test Cases for Calculating Area of Rectangle By Handling Exceptions

--- src/main/java/com/m2p/RectangleOperations.java
+++ src/main/java/com/m2p/RectangleOperations.java
@@ -1,4 +1,10 @@
 package com.m2p;
 
 public class RectangleOperations {
+    public int area(int length, int breadth) throws IllegalArgumentException{
+        if(length < 0 || breadth < 0) {
+            throw new IllegalArgumentException();
+        }
+        return length * breadth;
+    }
 }
--- src/test/java/com/m2p/RectangleOperationsTest.java
+++ src/test/java/com/m2p/RectangleOperationsTest.java
@@ -1,5 +1,28 @@
 package com.m2p;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class RectangleOperationsTest{
-    RectangleOperations rectangleOperations = new RectangleOperations();
+    RectangleOperations rectangle = new RectangleOperations();
+
+    @Test
+    void toReturnAreaOfRectangle()  {
+        // Arrange
+        int expectedArea = 100;
+
+        //Act
+        int actualArea = rectangle.area(10,10);
+
+        //Assert
+        assertEquals(expectedArea,actualArea);
+    }
+
+    @Test
+    void toReturnExceptionIfLengthOrBreadthIsNegative()
+    {
+        assertThrows(IllegalArgumentException.class,()-> rectangle.area(3,-3));
+    }
 }
